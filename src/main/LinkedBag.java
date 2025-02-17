67c36e9bb04a723936f92b91cb21c8d15a5b6422
Ryan Vu <vuryan612@gmail.com>
2025-02-17 13:19:14 -0800

    add temporary methods that need to be implemented

--- src/main/LinkedBag.java
+++ src/main/LinkedBag.java
@@ -1,4 +1,75 @@
 package main;
-public class LinkedBag<T> implements BagInterface<T> {
-        //demo the three methods in BagInterface
+
+public class LinkedBag<T> implements BagInterface {
+    @Override
+    public int getCurrentSize() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getCurrentSize'");
+    }
+
+    @Override
+    public boolean isEmpty() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'isEmpty'");
+    }
+
+    @Override
+    public boolean add(Object newEntry) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'add'");
+    }
+
+    @Override
+    public Object remove() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'remove'");
+    }
+
+    @Override
+    public boolean remove(Object anEntry) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'remove'");
+    }
+
+    @Override
+    public void clear() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'clear'");
+    }
+
+    @Override
+    public int getFrequencyOf(Object anEntry) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getFrequencyOf'");
+    }
+
+    @Override
+    public boolean contains(Object anEntry) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'contains'");
+    }
+
+    @Override
+    public Object[] toArray() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'toArray'");
+    }
+
+    @Override
+    public BagInterface union(BagInterface anotherBag) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'union'");
+    }
+
+    @Override
+    public BagInterface intersection(BagInterface anotherBag) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'intersection'");
+    }
+
+    @Override
+    public BagInterface difference(BagInterface anotherBag) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'difference'");
+    }
 }
